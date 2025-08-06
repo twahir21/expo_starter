98b3443d4465ca1244e7b5b5f1cc285fab5454ec
twahir21 <twahirsudy3@gmail.com>
2025-08-06 13:28:40 +0300

    implementing nativewind and link navigation pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-gray-500">
-      <Text className="text-white">Twahir</Text>
+    <View className="flex-1 items-center justify-center bg-secondary">
+      <Text className="text-light-100">Twahir</Text>
+      <Link href="/profile">Go to Profile</Link>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4F46E5",
+        secondary: "#F59E0B",
+        accent: "#10B981",
+        background: "#F3F4F6",
+        text: "#111827",
+        border: "#D1D5DB"
+      },
+      light: {
+        100: "#F3F4F6",
+      },
+      dark: {
+        100: "#111827",
+      }
+    },
   },
   plugins: [],
 }
